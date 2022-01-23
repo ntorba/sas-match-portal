4b4df96c3011dafbd510703494ffbc46390ee53b
ntorba <32570754+ntorba@users.noreply.github.com>
2022-01-23 15:17:44 -0500

    format manage.py and remove name arg from seeded users

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 from flask.cli import FlaskGroup
 
-from flask_app.app import create_app, db 
-from flask_app.models import Group, User 
+from flask_app.app import create_app, db
+from flask_app.models import Group, User
 
 cli = FlaskGroup(create_app=create_app)
+
 
 @cli.command()
 def recreate_db():
@@ -11,31 +12,29 @@ def recreate_db():
     db.create_all()
     db.session.commit()
 
+
 @cli.command()
 def seed_db():
     """Put some inital data in the database"""
     users = [
         User(
-            role="Admin",
-            name="admin",
-            email = "admin@admin.com",
-            password = "admins",
-            admin=True
+            role="admin",
+            email="admin@admin.com",
+            password="admins",
+            admin=True,
         ),
         User(
-            role="Teacher",
-            name="Mr. Joe",
-            email = "mrjoe@mrjoe.com",
-            password = "fakies",
-            admin=False
+            role="group_leader",
+            email="mrjoe@mrjoe.com",
+            password="fakies",
+            admin=False,
         ),
         User(
-            role="Scientist",
-            name="fakie scientist",
-            email = "science@science.com",
-            password = "fakies",
-            admin=False
-        )
+            role="scientist",
+            email="science@science.com",
+            password="fakies",
+            admin=False,
+        ),
     ]
     for user in users:
         db.session.add(user)
@@ -46,16 +45,17 @@ def seed_db():
             name="first classroom",
             school_district="the district",
             city="Philadelphia",
-            state="PA", 
+            state="PA",
             country="US",
             time_zone="US/Eastern",
             monday_start_time="09:00",
-            scientist_preferred_type="cool ones"
+            scientist_preferred_type="cool ones",
         )
     ]
     for classroom in classrooms:
         db.session.add(classroom)
     db.session.commit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
