d708df0397b8890792c3c6c98f9228f5fad1b6a6
ntorba <32570754+ntorba@users.noreply.github.com>
2022-01-24 17:16:46 -0500

    update test_auth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,6 +5,16 @@ from flask_app.views.forms import RegisterUserForm, LoginForm
 from flask_app.extensions import db
 
 
+def login(client, email, password):
+    return client.post(
+        "/login", data=dict(email=email, password=password), follow_redirects=True
+    )
+
+
+def logout(client):
+    return client.get("/logout", follow_redirects=True)
+
+
 def add_user(role, email, password):
     user = User(role=role, email=email, password=password)
     db.session.add(user)
@@ -28,29 +38,19 @@ def test_registered_user_login(client, session):
     email = "test_scientist@science.com"
     password = "fake_password"
     scientist = add_user(role, email, password)
-    role = "group_leader"
-    email = "test_gl@gl.com"
-    gl = add_user(role, email, password)
 
-    scientist_form = LoginForm(**{"email": scientist.email, "password": password})
+    login_res = login(client, email, password)
+    assert (
+        login_res.status_code == 200
+    ), "expected 200 status code for login response"  # TODO: I Guess it changes this from 302 to 200 internally, not sure why?
 
-    response = client.post("/login", data=scientist_form.data)
+    home_res = client.get("/home")
     assert (
-        response.status_code == 302
-    ), f"Expected redirect status_code 302, but got {response.status_code} instead."
-    assert (
-        "http://localhost/home" == response.headers["Location"]
-    ), "login response header did not redirect to /home as expected"
+        home_res.status_code == 200
+    ), f"expected status_code 200 for logged in users accessing /home, but got {home_res.status_code}"
 
-    teacher_form = LoginForm(**{"email": gl.email, "password": password})
-
-    response = client.post("/login", data=teacher_form.data)
-    assert (
-        response.status_code == 302
-    ), f"Expected redirect status_code 302, but got {response.status_code} instead."
-    assert (
-        "http://localhost/home" == response.headers["Location"]
-    ), "login response header did not redirect to /home as expected"
+    logout_res = logout(client)
+    assert logout_res.status_code == 200, "Logout status code should have returned 200"
 
 
 def test_login_nonexistent_email(client, session):
@@ -85,8 +85,9 @@ def test_login_bad_password(client, session):
     )
 
     response = client.post("/login", data=scientist_form.data)
-    assert re.search(  # TODO: Do a more robust check by giving flashed messages an idea in html, make beautiful soup object and find those messages specifically
-        "This password does not match email.", response.get_data(as_text=True)
+    # TODO: Do a more robust check by giving flashed messages an idea in html, make beautiful soup object and find those messages specifically
+    assert (
+        response.get_data(as_text=True) == "This password does not match email."
     ), "did not find 'This password does not match email.' in response body"
     assert (
         response.status_code == 200
@@ -163,10 +164,9 @@ def test_register_scientist(client, session):
     assert user.role.name == role
 
 
-def test_register_scientist_mismatched_emails(client, session):
+def test_register_scientist_mismatched_emails(client, session, app):
     # test that viewing the page renders without template errors
     assert client.get("/register").status_code == 200
-
     role = "scientist"
     email = "test_scientist@science.com"
     wrong_email = "no@no.com"
@@ -185,15 +185,15 @@ def test_register_scientist_mismatched_emails(client, session):
         "/register",
         data=form.data,
     )
-    assert re.search(  # TODO: Do a more robust check by giving flashed messages an idea in html, make beautiful soup object and find those messages specifically
-        "Make sure your email entries match", response.get_data(as_text=True)
-    ), "did not find wrong email flashed message in response body"
+    # TODO: do more research to see if I can access to the active view of the html despirte using turbo streams
+    # with this strategy, I can be sure that turbo.push ran in the view file, but it would be better to access the actual html
+    assert (
+        response.get_data(as_text=True) == "Make sure your email entries match"
+    ), "Received an unexpected response error for mismatched emails"
 
-    # make sure there is no message about passwords mismatching
-    assert not re.search(  # TODO: Do a more robust check by giving flashed messages an idea in html, make beautiful soup object and find those messages specifically
-        "Passwords must match.", response.get_data(as_text=True)
-    ), "found unexpected error for mismatched passwords"
-
+    assert (
+        response.status_code == 200
+    ), "expected reponse code 200 for mismatched password, success sending back turbo-stream"
     assert (
         "Location" not in response.headers
     ), "registration response tried to redirect when it should have failed"
@@ -212,7 +212,7 @@ def test_register_group_leader_mismatched_password(client, session):
     role = "group_leader"
     email = "test_scientist@science.com"
     password = "password"
-    wrong_password = "no"
+    wrong_password = "noooooooo"
     # test that successful registration redirects to the login page
     form = RegisterUserForm(
         **{
@@ -227,13 +227,18 @@ def test_register_group_leader_mismatched_password(client, session):
         "/register",
         data=form.data,
     )
-    assert re.search(  # TODO: Do a more robust check by giving flashed messages an idea in html, make beautiful soup object and find those messages specifically
-        "Passwords must match.", response.get_data(as_text=True)
-    ), "did not find wrong email flashed message in response body"
+    # TODO: do more research to see if I can access to the active view of the html despirte using turbo streams
+    # with this strategy, I can be sure that turbo.push ran in the view file, but it would be better to access the actual html
+    assert (
+        response.get_data(as_text=True) == "Passwords must match."
+    ), "Received an unexpected response error for mismatched passwords"
+
+    assert (
+        response.status_code == 200
+    ), "expected reponse code 200 for mismatched password, success sending back turbo-stream"
     assert (
         "Location" not in response.headers
     ), "registration response tried to redirect when it should have failed"
-
     # test that the user was inserted into the database
     user = User.query.filter(User.email == email).first()
     assert (
@@ -266,12 +271,16 @@ def test_register_group_leader_mismatched_password_and_mismatched_email(
         "/register",
         data=form.data,
     )
-    assert re.search(  # TODO: Do a more robust check by giving flashed messages an idea in html, make beautiful soup object and find those messages specifically
-        "Make sure your email entries match", response.get_data(as_text=True)
-    ), "did not find wrong email flashed message in response body"
-    assert re.search(  # TODO: Do a more robust check by giving flashed messages an idea in html, make beautiful soup object and find those messages specifically
-        "Passwords must match.", response.get_data(as_text=True)
-    ), "did not find wrong email flashed message in response body"
+    assert (
+        response.get_data(as_text=True)
+        == "Make sure your email entries match, Passwords must match."
+    )
+    # assert re.search(  # TODO: Do a more robust check by giving flashed messages an idea in html, make beautiful soup object and find those messages specifically
+    #     "Make sure your email entries match", response.get_data(as_text=True)
+    # ), "did not find wrong email flashed message in response body"
+    # assert re.search(  # TODO: Do a more robust check by giving flashed messages an idea in html, make beautiful soup object and find those messages specifically
+    #     "Passwords must match.", response.get_data(as_text=True)
+    # ), "did not find wrong email flashed message in response body"
     assert (
         "Location" not in response.headers
     ), "registration response tried to redirect when it should have failed"
@@ -317,31 +326,21 @@ def test_register_scientist_duplicate_email(client, session):
     ), f"expected status_code 200 because of failed registration, but got {response.status_code}"
 
 
-def login(client, email, password):
-    return client.post(
-        "/login", data=dict(email=email, password=password), follow_redirects=True
-    )
+# def test_registered_logout(client, app, session):
+#     user = add_user("scientist", "test_scientist@science.com", "fake_password")
 
+#     rv = login(client, "test_scientist@science.com", "fake_password")
+#     assert (
+#         b"You've successfully logged in." in rv.data
+#     ), "success flash message isn't shown"
 
-def logout(client):
-    return client.get("/logout", follow_redirects=True)
-
-
-def test_registered_logout(client, app, session):
-    user = add_user("scientist", "test_scientist@science.com", "fake_password")
-
-    rv = login(client, "test_scientist@science.com", "fake_password")
-    assert (
-        b"You've successfully logged in." in rv.data
-    ), "success flash message isn't shown"
-
-    response = client.get("/home")
-    assert (
-        response.status_code == 302
-    ), f"Expected redirect status_code 302, but got {response.status_code} instead."
-    assert (
-        "http://localhost/home" == response.headers["Location"]
-    ), "login response header did not redirect to /home as expected"
+#     response = client.get("/home")
+#     assert (
+#         response.status_code == 302
+#     ), f"Expected redirect status_code 302, but got {response.status_code} instead."
+#     assert (
+#         "http://localhost/home" == response.headers["Location"]
+#     ), "login response header did not redirect to /home as expected"
 
 
 # def test_add_category_post(app, session):
