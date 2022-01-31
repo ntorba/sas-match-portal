ce15d505da594f917225f99433609b218caaf621
ntorba <32570754+ntorba@users.noreply.github.com>
2022-01-31 07:54:07 -0500

    trying with a link instead

---
+++ frontend/src/application/profileUpdateForm.js
@@ -0,0 +1,9 @@
+
+
+var profileUpdateDiv = document.querySelector("#profileUpdateFormDiv");
+
+profileUpdateDiv.addEventListener('click', function (e) {
+    console.log("here is the event: ");
+    console.log(e);
+    e.preventDefault();
+});
