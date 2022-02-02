b2abc67b47e6766d006d7c05cd8f46ae77f01ffe
ntorba <32570754+ntorba@users.noreply.github.com>
2022-02-02 11:22:18 -0500

    Update main.py

--- flask_app/views/main.py
+++ flask_app/views/main.py
@@ -3,9 +3,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 
 from ..models import User, Match
 from ..extensions import bcrypt, db
-from .forms import (
-    ChangePasswordForm,
-)
+from .forms import ChangePasswordForm, GroupLeaderUpdateProfileForm
 
 main_blueprint = Blueprint("main", __name__, template_folder="templates")
 
@@ -35,7 +33,40 @@ def home():
 @main_blueprint.route("/profile")
 @login_required
 def profile():
-    return render_template("profile.html")
+    return render_template(
+        "profile-group-leader.html",
+        current_user_name=current_user.name,
+        current_user_email=current_user.email,
+    )
+
+
+@main_blueprint.route("/profile/update", methods=["GET", "POST"])
+@login_required
+def profile_update_form():
+    form = GroupLeaderUpdateProfileForm(request.form)
+    if request.method == "POST" and form.validate_on_submit():
+        user = User.query.filter(User.email == current_user.email).first()
+        for field, value in form.data.items():
+            setattr(user, field, value)
+        db.session.commit()
+        # return render_template("profile-group-leader-edit-link.html")
+        return redirect(url_for("main.profile"))
+    elif request.method == "POST":
+        # TODO: push errors with turbo streams
+        flash("errors with profile update")
+        pass
+    else:
+        return render_template(
+            "profile-group-leader-update-form.html",
+            form=form,
+            current_name=current_user.name,
+            current_email=current_user.email,
+        )
+
+
+@main_blueprint.route("/profile/dissapear", methods=["GET"])
+def profile_deissapear():
+    return render_template("profile-group-leader-edit-link.html")
 
 
 @main_blueprint.route("/profile_update", methods=["GET", "POST"])
