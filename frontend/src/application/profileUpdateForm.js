e4a4c9fa78bfddb11bdf3e67e0ba40aec828d3dc
ntorba <32570754+ntorba@users.noreply.github.com>
2022-02-02 11:20:11 -0500

    Update profileUpdateForm.js

--- frontend/src/application/profileUpdateForm.js
+++ frontend/src/application/profileUpdateForm.js
@@ -5,5 +5,5 @@ var profileUpdateDiv = document.querySelector("#profileUpdateFormDiv");
 profileUpdateDiv.addEventListener('click', function (e) {
     console.log("here is the event: ");
     console.log(e);
-    e.preventDefault();
+    // e.preventDefault();
 });
